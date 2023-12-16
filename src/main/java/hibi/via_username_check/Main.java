af1181ab964474961067918330d6f70df3aa2698
hibi <69405847+hibiii@users.noreply.github.com>
2023-12-16 17:53:21 -0300

    add cli argument

--- src/main/java/hibi/via_username_check/Main.java
+++ src/main/java/hibi/via_username_check/Main.java
@@ -1,19 +1,29 @@
 package hibi.via_username_check;
 
+import joptsimple.OptionSpec;
 import net.lenni0451.lambdaevents.EventHandler;
 import net.raphimc.viaproxy.ViaProxy;
 import net.raphimc.viaproxy.plugins.ViaProxyPlugin;
 import net.raphimc.viaproxy.plugins.events.ClientLoggedInEvent;
+import net.raphimc.viaproxy.plugins.events.PostOptionsParseEvent;
+import net.raphimc.viaproxy.plugins.events.PreOptionsParseEvent;
 
 public class Main extends ViaProxyPlugin {
 
+    boolean disabled;
+    OptionSpec<Void> disableUsernameCheck;
+
     @Override
     public void onEnable() {
+        disabled = false;
         ViaProxy.EVENT_MANAGER.register(this);
     }
 
     @EventHandler
     public void clientLoggedIn(ClientLoggedInEvent ev) {
+        if (disabled) {
+            return;
+        }
         final var con = ev.getProxyConnection();
         final var acc = con.getUserOptions().account();
         if (acc == null) {
@@ -23,6 +33,19 @@ public class Main extends ViaProxyPlugin {
         final var actual = con.getGameProfile().getName();
         if (!actual.equalsIgnoreCase(expected)) {
             con.kickClient("§cUsername mismatch! You are logging in as "+actual+", but the selected account in the proxy is "+expected+".");
+        }
+    }
+
+    @EventHandler
+    public void addCliOption(PreOptionsParseEvent ev) {
+        disableUsernameCheck = ev.getParser().accepts("disable_username_check", "Disables username verification of the connecting client against the selected account.");
+    }
+
+    @EventHandler
+    public void applyOption(PostOptionsParseEvent ev) {
+        final var opts = ev.getOptions();
+        if (opts.has(disableUsernameCheck)) {
+            disabled = true;
         }
     }
 }
