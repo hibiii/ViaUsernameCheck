563f5b1a89d87f65de7830daca021fc3951535d0
hibi <69405847+hibiii@users.noreply.github.com>
2023-12-16 15:02:57 -0300

    hello, world

---
+++ src/main/java/hibi/via_username_check/Main.java
@@ -0,0 +1,11 @@
+package hibi.via_username_check;
+
+import net.raphimc.viaproxy.plugins.ViaProxyPlugin;
+
+public class Main extends ViaProxyPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("[ViaUsernameCheck] hello, world");
+    }
+}
